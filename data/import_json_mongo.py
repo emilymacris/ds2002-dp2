16b10ae0be55a03e80601cf96d14c9b3d54a2495
Emily Macris <bdf7bz@virginia.edu>
2024-04-18 20:47:10 +0000

    trying this

---
+++ data/import_json_mongo.py
@@ -0,0 +1,58 @@
+#!/home/gitpod/.pyenv/shims/python3
+
+import pymongo
+from pymongo import MongoClient, errors
+from bson.json_util import dumps
+import os
+import json
+
+MONGOPASS = os.getenv('MONGOPASS')
+uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
+client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+# specify a database
+db = client.bdf7bz
+# specify a collection
+collection = db.jsondocs
+
+path = "."
+
+for (root, dirs, file) in os.walk(path):
+    for f in file:
+        with open(f) as file:
+            try:
+                file_data = json.load(file)
+            except Exception as e:
+                print(file, "has an error", e)
+                continue
+            if isinstance(file_data, list):
+                collection.insert_many(file_data)  
+            else:
+                collection.insert_one(file_data)
+
+
+
+# assuming you have defined a connection to your db and collection already:
+
+
+     
+# Inserting the loaded data in the collection
+# if JSON contains data more than one entry
+# insert_many is used else insert_one is used
+
+
+
+
+# import pymongo
+# from pymongo import MongoClient, errors
+# from bson.json_util import dumps
+# import os
+# import json
+
+# MONGOPASS = os.getenv('MONGOPASS')
+# uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
+# client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+# # specify a database
+# db = client.bdf7bz
+# # specify a collection
+# collection = db.json-data
+
