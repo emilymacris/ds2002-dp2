bf620f99d6f7fd3ddce8beacea9c67545628d243
Emily Macris <bdf7bz@virginia.edu>
2024-04-23 20:33:45 +0000

    trying

--- data/import_json_mongo.py
+++ data/import_json_mongo.py
@@ -8,9 +8,6 @@ import json
 
 #remember to use MONGO-ATLAS to connect to shell
 
-
-
-
 MONGOPASS = os.getenv('MONGOPASS')
 uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
 client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
@@ -18,6 +15,13 @@ client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutM
 db = client.bdf7bz
 # specify a collection
 collection = db.jsondocs
+
+
+# return RECORDS IMPORTED OK
+# GOOD RECORDS NOT IMPORTED - like if one in file ?
+# how many records were corrupted 
+# 
+
 
 path = "."
 
@@ -29,7 +33,7 @@ for (root, dirs, file) in os.walk(path):
             except Exception as e:
                 print(file, "has an error", e)
                 continue
-            if isinstance(file_data, list):gi
+            if isinstance(file_data, list):
                 collection.insert_many(file_data)  
             else:
                 collection.insert_one(file_data)
